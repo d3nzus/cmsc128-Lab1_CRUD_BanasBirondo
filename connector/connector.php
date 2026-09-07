4bff8367443a97026cd46f4dc0d1a0e385fe74a9
Renz Frederick <d3nzus@gmail.com>
2026-09-07 19:58:52 +0800

    test: tailwind components in index.php

--- connector/connector.php
+++ connector/connector.php
@@ -1,4 +1,7 @@
 <?php
+
+    require_once __DIR__ . '/../connector/connector.php';
+
     $conn = new mysqli($servername, $username, $password); // Connect without specifying a database
     
     if ($conn->connect_error) {
