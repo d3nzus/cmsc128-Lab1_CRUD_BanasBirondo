7b16ca17714fa4a36804465eff015e7fe1355f01
Yuan Birondo <ycbirondo@up.edu.ph>
2026-09-08 02:42:33 +0800

    fix: display category names

--- components/tasklist.php
+++ components/tasklist.php
@@ -3,7 +3,15 @@
 <?php
 require_once __DIR__ . '/../connector/connector.php';
 
-$sql = "SELECT * FROM task";
+$sql = "SELECT
+task.title,
+task.id,
+task.due_date,
+task.due_time,
+task.priority,
+category.name
+FROM task
+LEFT JOIN category ON task.category_id = category.id";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -13,7 +21,7 @@ if ($result->num_rows > 0) {
             <td>{$row["due_date"]}</td>
             <td>{$row["due_time"]}</td>
             <td>{$row["priority"]}</td>
-            <td>{$row["category_id"]}</td>";
+            <td>{$row["name"]}</td>";
         echo "</tr>";
     }
 }
