6eaaee95a3c4ce3635762685d040f9da00412a74
Renz Frederick <d3nzus@gmail.com>
2026-09-08 00:46:48 +0800

    fix: bug with connector

--- connector/connector.php
+++ connector/connector.php
@@ -2,7 +2,7 @@
 
     require_once __DIR__ . '/../config/config.php';
 
-    $conn = new mysqli($servername, $username, $password); // Connect without specifying a database
+    $conn = new mysqli($servername, $username, $password, $dbname);
     
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
