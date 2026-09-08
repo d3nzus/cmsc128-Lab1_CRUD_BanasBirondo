8de7278570f950582b711212ca6d339e30a571fa
Renz Frederick <d3nzus@gmail.com>
2026-09-09 00:34:50 +0800

    refactor: self-contain archiving and undo

--- components/undoButton.php
+++ components/undoButton.php
@@ -1,8 +1,6 @@
 <?php
-require_once __DIR__ . '/../helpers/trash.php';
-
-    echo '<form action="../helpers/trash.php" method="post">
+    echo '<form action="../helpers/undoDelete.php" method="post">
         <input type="hidden" name="action" value="undo">
-        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Undo Delete</button>
+        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Undo</button>
     </form>';
 ?>
