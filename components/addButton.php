f95d63236faf5138d0a469f80b0b5f8c50e3f912
Renz Frederick <d3nzus@gmail.com>
2026-09-09 01:15:27 +0800

    refactor: seperate page for addTask

---
+++ components/addButton.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<form class = 'actions' action='addForm.php' method = 'post'>
+                    <input type = 'text' style = 'display:none' name = 'action' value = 'add'>
+                    <button type='submit' class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add Task</button>
+                </form>";
+?>
